74f4128fcc233c1dc79fc8211dc9628020b323f2
evannorton <evanan39@yahoo.com>
2021-05-26 19:48:10 -0400

    base

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ const aspect = width / height;
 
 const gameElement = document.getElementById("game");
 
+const setImageDimensions = () => {
+    for (const elm of document.getElementsByTagName("img")) {
+        elm.width = elm.width;
+        elm.height = elm.height;
+    }
+};
+
 const sizeGame = () => {
     const scale = innerWidth / innerHeight > aspect ? innerHeight / height : innerWidth / width;
     gameElement.style.width = `${width * scale}px`;
     gameElement.style.height = `${height * scale}px`;
+    for (const elm of document.getElementsByTagName("img")) {
+        elm.style.width = `${elm.getAttribute("width") * scale}px`;
+        elm.style.height = `${elm.getAttribute("height") * scale}px`;
+    }
+    document.getElementById("base").style.left = `${16 * scale}px`;
 };
 
 const onWindowResize = () => {
@@ -16,4 +28,5 @@ const onWindowResize = () => {
 
 addEventListener("resize", onWindowResize);
 
+setImageDimensions();
 sizeGame();
